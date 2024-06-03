4e33f0d9510a78ee2b1eeae061fafdf9ecbd6c29
Srishty M <srishtymangutte@gmail.com>
2024-06-03 19:56:59 +0530

    feat: added navigation for routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Playground from "./pages/Playground.js";
+import NavBar from "./components/marginals/NavBar.jsx";
 
 const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/playground">Playground</Link>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/playground" element={<Playground />} />
--- src/components/marginals/NavBar.jsx
+++ src/components/marginals/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
 const NavBar = () => {
