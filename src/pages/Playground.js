22a9302708436a36375bc66c9e93c32e15ae7c9e
Srishty M <srishtymangutte@gmail.com>
2024-06-05 02:50:54 +0530

    chore: added src

---
+++ src/pages/Playground.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Playground = () => {
+  return (
+    <div>
+      <h1>Playground page</h1>
+      <p>For testing purpose</p>
+    </div>
+  );
+};
+
+export default Playground;
