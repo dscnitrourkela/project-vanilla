d45a273410324da8a62c184ef75f6adc23d440fd
ayussh-2 <ayushatschool5@gmail.com>
2024-06-06 16:00:08 +0530

    added recommended changes

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -3,8 +3,6 @@ import About from '../components/about/About'
 const Playground = () => {
   return (
     <div>
-      {/* <h1>Playground page</h1>
-      <p>For testing purpose</p> */}
       <About />
     </div>
   )
