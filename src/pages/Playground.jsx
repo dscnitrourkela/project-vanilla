ba664b2ab3bb971067312cb7f8bed15d05db85c1
Harsh <harshtanishq2002@gmail.com>
2024-06-09 12:44:07 +0530

    fix: ran prettier fix, added carousel extension

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -1,12 +1,9 @@
-
 import NavHero from '../components/marginals/Nav_Hero/NavHero'
 import { TeamCarousel } from '../components/teamMembers/teamCarousel'
-
 
 const Playground = () => {
   return (
     <div>
-      <h1>Hello</h1>
       <h1>Playground page</h1>
       <p>For testing purpose</p>
       <NavHero />
