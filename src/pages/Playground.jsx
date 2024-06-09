995e78272bb81f053116c82c6c3689eebe1ddc2e
ayussh-2 <ayushatschool5@gmail.com>
2024-06-09 15:29:45 +0530

    Added navbar and hero

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -1,9 +1,6 @@
-
 import About from '../components/about/About'
 import Events from '../components/events/Events'
-
 import NavHero from '../components/marginals/Nav_Hero/NavHero'
-
 import { Sponsors } from '../components/sponsors/sponsors'
 import { TeamCarousel } from '../components/teamMembers/teamCarousel'
 import Footer from '../components/marginals/footer/Footer'
@@ -12,7 +9,6 @@ const Playground = () => {
   return (
     <div>
       <h1>Playground page</h1>
-
       <p>For testing purposes</p>
       <NavHero />
       <About />
