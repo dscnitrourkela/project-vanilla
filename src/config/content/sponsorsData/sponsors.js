8d610d776523846b89662bf8d7f53cb6468998f5
ayanSCYY <ayanm3206@gmail.com>
2024-06-09 13:38:15 +0530

    feat: add sponsor carousal

---
+++ src/config/content/sponsorsData/sponsors.js
@@ -0,0 +1,28 @@
+import tw from 'twin.macro'
+
+export const SponsorsData = [
+  {
+    styles: tw`max-md:m-[70px] md:w-[220px] md:h-[40px] md:m-[110px] `,
+    url: 'https://res.cloudinary.com/dfe8sdlkc/image/upload/v1717761140/Logo_jc3ia8.png'
+  },
+  {
+    styles: tw`max-md:m-[70px] md:w-[202px] md:h-[40px] md:m-[110px] `,
+    url: 'https://res.cloudinary.com/dfe8sdlkc/image/upload/v1717761140/Logo-1_vqmsdg.png'
+  },
+  {
+    styles: tw`max-md:m-[70px] md:w-[100px] md:h-[38px] md:m-[110px] `,
+    url: 'https://res.cloudinary.com/dfe8sdlkc/image/upload/v1717761140/Logo-2_wzvo0w.png'
+  },
+  {
+    styles: tw`max-md:m-[70px] md:w-[80px]  md:h-[80px] md:m-[110px] `,
+    url: 'https://res.cloudinary.com/dfe8sdlkc/image/upload/v1717761140/Logo-3_qxz0na.png'
+  },
+  {
+    styles: tw`max-md:m-[70px] md:w-[180px] md:h-[40px] md:m-[110px] `,
+    url: 'https://res.cloudinary.com/dfe8sdlkc/image/upload/v1717761141/Logo-4_yvqmg4.png'
+  },
+  {
+    styles: tw`max-md:m-[70px] md:w-[120px] md:h-[40px] md:m-[110px] `,
+    url: 'https://res.cloudinary.com/dfe8sdlkc/image/upload/v1717761144/Logo-5_fnneio.png'
+  }
+]
