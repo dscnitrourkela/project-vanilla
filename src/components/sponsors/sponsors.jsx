c85153fa29358b715b4eb8a692c194865640d248
ayanSCYY <ayanm3206@gmail.com>
2024-06-09 12:29:42 +0530

    feat: update prettier fix

--- src/components/sponsors/sponsors.jsx
+++ src/components/sponsors/sponsors.jsx
@@ -1,17 +1,16 @@
-import { Icon, SponsorsContainer ,IconContainer} from './sponsors.styles'
+import { Icon, SponsorsContainer, IconContainer } from './sponsors.styles'
 import Marquee from 'react-fast-marquee'
 import { SponsorsData } from '../../config/content/mycomp_data/sponsors'
 
 export const Sponsors = () => {
   return (
     <SponsorsContainer>
-      <Marquee> 
+      <Marquee>
         {SponsorsData.map((data, index) => (
-          <IconContainer key={index} >
-             <Icon key={index} style={data.styles} src={data.url}  />
+          <IconContainer key={index}>
+            <Icon key={index} style={data.styles} src={data.url} />
           </IconContainer>
         ))}
-        
       </Marquee>
     </SponsorsContainer>
   )
--- src/components/sponsors/sponsors.styles.jsx
+++ src/components/sponsors/sponsors.styles.jsx
@@ -5,11 +5,10 @@ export const SponsorsContainer = styled.div`
   ${tw`bg-brown-2 h-[410px] pt-[70px] `}
 `
 
-export const IconContainer=styled.span(({style  }) => ({
+export const IconContainer = styled.span(({ style }) => ({
   ...style
-  
 }))
-export const Icon = styled.img(({ src,style }) => ({
-   src: `url(${src})`,
-   ...style
+export const Icon = styled.img(({ src, style }) => ({
+  src: `url(${src})`,
+  ...style
 }))
