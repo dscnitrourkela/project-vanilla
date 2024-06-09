3fd7f399d53469e73cc65c3f961a0aff999ebdb4
Dhrutinandan Swain <92504849+swaindhruti@users.noreply.github.com>
2024-06-09 10:54:36 +0530

    feat: add Team Carousel Component (#11)
    * feat: add team carousel
    
    * chore: Update font family to use "Poppins" in teamCards.styles.jsx and tailwind.config.js
    
    * chore: prettier fix
    
    * chore: change in index.css
    
    * chore: rectify typo
    
    * chore: changes were made as per the suggestions in the review
    
    * refactor: update path for team data file
    
    * chore: refactor break points
    
    * chore: add netlify.toml
    
    * chore: change team data path
    
    * chore: remove pnpm-lock.yaml
    
    * chore: add new cloudinary image links
    
    ---------
    
    Co-authored-by: anmol5936 <127973342+anmol5936@users.noreply.github.com>

---
+++ src/components/teamMembers/teamSection.styles.jsx
@@ -0,0 +1,10 @@
+import tw from 'twin.macro'
+import styled from 'styled-components'
+
+export const TeamSectionContainer = styled.div`
+  ${tw`bg-brown-2 md:h-[700px] pl-3 pr-3`}
+`
+
+export const TeamSectionHeading = styled.div`
+  ${tw`font-Poppins text-brown-4 text-[40px] font-bold 3md:text-[128px] 2.5md:text-[90px] 0.7xsm:text-[60px]`}
+`
