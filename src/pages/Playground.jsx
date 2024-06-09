1e658bdf5d1d2e4e751e847ff5cf006ae43b8062
Harsh <harshtanishq2002@gmail.com>
2024-06-09 12:30:24 +0530

    fix:playground changes

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -3,6 +3,7 @@ import NavHero from '../components/marginals/Nav_Hero/NavHero'
 const Playground = () => {
   return (
     <div>
+      <h1>Hello</h1>
       <h1>Playground page</h1>
       <p>For testing purpose</p>
       <NavHero />
