2ed83eeb87508985d45cbb080f5a3b9f12e789d0
Dhrutinandan <dhrutiswainnandan@gmail.com>
2024-06-10 22:30:44 +0530

    chore: reduce gap between timeline and registrationHelp sections

--- src/components/timeline/timeLine.styles.jsx
+++ src/components/timeline/timeLine.styles.jsx
@@ -2,7 +2,7 @@ import tw from 'twin.macro'
 import styled from 'styled-components'
 
 export const TimelineContainer = styled.div`
-  ${tw`bg-brown-2 mb-12 3md:mb-20 pl-3`}
+  ${tw`bg-brown-2 mb-12 3md:mb-14 pl-3`}
 `
 export const TimelineHeading = styled.div`
   ${tw`font-Poppins text-brown-4 text-[40px] font-bold 3md:text-[128px] 2.5md:text-[90px] 0.7xsm:text-[60px]`}
