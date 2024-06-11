b801d6c10d3f02f6c40a724bcaf5958d7778e63e
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2024-06-11 17:47:21 +0530

    chore: reduce the gap betwwen timeline and registrationhelp section in mobile view

--- src/components/timeline/timeLine.styles.jsx
+++ src/components/timeline/timeLine.styles.jsx
@@ -2,7 +2,7 @@ import tw from 'twin.macro'
 import styled from 'styled-components'
 
 export const TimelineContainer = styled.div`
-  ${tw`bg-brown-2 mb-12 3md:mb-14 pl-3`}
+  ${tw`bg-brown-2 mb-5 3md:mb-14 pl-3`}
 `
 export const TimelineHeading = styled.div`
   ${tw`font-Poppins text-brown-4 text-4.5xl font-bold 3md:text-9xl 2.5md:text-7.5xl 0.7xsm:text-6xl`}
