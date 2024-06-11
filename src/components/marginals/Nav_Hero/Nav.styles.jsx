3f2c4a909509bac44d8780d6b0dbba8ecadb5e71
Harsh <140698710+Cybrite@users.noreply.github.com>
2024-06-11 19:18:46 +0530

    feat: Added register button to navbar (#26)
    * feat: added register button, modified hamburger
    fix: gave margin in Register now , hero icon fragmented
    
    * fix:margin added
    
    * fix: fixed Hamburger-Logo alignment
    
    * fix:fixed Register Vertical gap
    
    * fix: breakpoint issue
    
    * fix: breakpoint fix
    
    * fix: responsive design, added margin in hambergur menu
    
    * fix: Register length
    
    * fix: minor content changes, register size
    
    * fix: Date-and-Time Card

--- src/components/marginals/Nav_Hero/Nav.styles.jsx
+++ src/components/marginals/Nav_Hero/Nav.styles.jsx
@@ -20,21 +20,32 @@ export const InnrNavCont = styled.div`
 
 export const Logo = styled.div`
   ${tw`
-  h-[48px]
-  w-[48px] 
+  h-[80px]
+  md:w-[160px]
   sm:h-[40px] 
-  sm:w-[40px] 
+  sm:w-[80px] 
   md:h-[54px] 
-  md:w-[54px]
   cursor-pointer
   z-50
+  flex
   `}
 `
 export const LogoImg = styled.img`
-  ${tw`object-contain w-full h-full `}
+  @media (max-width: 573px) {
+    margin-top: 10px;
+  }
+  ${tw`object-contain w-full sm:h-full h-[65%] `}
 `
+export const NitImg = styled.img`
+  @media (max-width: 573px) {
+    margin-top: 10px;
+    margin-right: 5px;
+  }
+  ${tw`object-contain w-full sm:h-[130%] ml-3 h-[80%]`}
+`
+
 export const Menu = styled.div`
-  ${tw`items-center hidden min-[903px]:flex`}
+  ${tw`items-center hidden min-[1090px]:flex`}
 `
 export const InnerMenu = styled.div`
   ${tw`
@@ -56,8 +67,49 @@ export const MenuItem = styled.div`
   `}
 `
 
+export const Register = styled.div`
+  @media (max-width: 671px) {
+    width: 20%;
+    height: 50px;
+  }
+  @media (max-width: 1090px) {
+    display: none;
+  }
+  ${tw`
+  w-[150px] 
+  h-[60px] 
+  font-semibold 
+  text-2xl 
+  bg-orange-1 
+  text-black-1 
+  font-Poppins 
+  rounded-2xl 
+  flex 
+  justify-evenly 
+  items-center 
+  cursor-pointer 
+  sm:mt-4
+  `}
+`
+export const SecRegister = styled.div`
+  ${tw`
+  w-[120px] 
+  h-[50px] 
+  font-semibold 
+  text-2xl 
+  bg-orange-1 
+  text-black-1 
+  font-Poppins 
+  rounded-2xl 
+  flex 
+  justify-evenly 
+  items-center 
+  cursor-pointer 
+  mt-4
+  `}
+`
 export const Menu2 = styled.div`
-  ${tw`min-[903px]:hidden items-center flex z-50`}
+  ${tw`min-[1090px]:hidden items-center flex z-50`}
 `
 
 export const MenuIcon = styled.div`
@@ -66,13 +118,13 @@ export const MenuIcon = styled.div`
 
 export const ResMen = styled.div`
   background-image: url(${links.stars});
-  ${tw`z-10 min-[903px]:hidden h-screen grid place-items-center w-full`}
+  ${tw`z-10 min-[1090px]:hidden h-screen grid place-items-center w-full`}
 `
 export const ResList = styled.ul`
   ${tw`flex flex-col items-center fixed top-0 left-0 h-full w-full justify-center gap-10`}
 `
 export const ResItem = styled.li`
-  ${tw` mb-3 cursor-pointer`}
+  ${tw` mb-1 cursor-pointer`}
 `
 
 export const ResAnchor = styled.a`
