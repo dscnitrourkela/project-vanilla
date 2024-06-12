60f6c3f279681d722968a3c5237994fb7f813067
ayussh-2 <ayushatschool5@gmail.com>
2024-06-12 16:29:44 +0530

    feat: added Registartion page

---
+++ src/components/registrationForm/InputField.styles.jsx
@@ -0,0 +1,43 @@
+import tw from 'twin.macro'
+import styled from 'styled-components'
+export const Input = styled.input`
+  ${tw`w-full h-10 px-2 bg-brown-2 text-brown-4 focus:outline-none font-normal`}
+  border-radius: 8px;
+  border: 1.6px solid black;
+  line-height: 1.2;
+  letter-spacing: 0.1em;
+  position: relative;
+  z-index: 1;
+`
+export const OuterBorder = styled.div`
+  position: relative;
+  width: 100%;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    right: -4px;
+    bottom: -4px;
+    border: 1.6px solid black;
+    border-radius: 8px;
+    z-index: -1;
+  }
+`
+export const InputContainer = styled.div`
+  ${tw`flex flex-col justify-start w-full gap-1`}
+`
+export const Label = styled.label`
+  ${tw`text-brown-4 font-[600] text-lg`}
+  letter-spacing: 1.036px;
+`
+export const ExtrasContainer = styled.div`
+  ${tw`flex items-center gap-2 mt-1`}
+`
+export const Info = styled.img`
+  ${tw`w-5 h-5`}
+`
+export const InfoBtn = styled.button`
+  ${tw`w-5 h-5 top-3 right-2 absolute z-10`}
+`
---
+++ src/components/registrationForm/RegisterForm.styles.jsx
@@ -0,0 +1,53 @@
+// RegisterForm.styles.js
+import tw from 'twin.macro'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  ${tw`grid md:grid-cols-2 grid-cols-1 font-Poppins`}
+`
+
+export const BackgroundImage = styled.div`
+  ${tw`bg-cover bg-center h-full w-full  z-0`}
+  background-image: url(${(props) => props.src});
+`
+export const LogoSection = styled.section`
+  ${tw`md:flex flex-col justify-center items-center h-full w-full relative hidden `}
+`
+export const LogoContainer = styled.div`
+  ${tw`flex flex-col items-center justify-center absolute z-10`}
+`
+export const Date = styled.h1`
+  ${tw`text-brown-2 2.7md:text-4xl font-semibold text-3xl`}
+`
+export const FormContainer = styled.form`
+  ${tw`flex flex-col justify-center items-center h-full w-full gap-[24px] md:gap-4 py-5 md:py-10 px-4 md:px-14 `}
+`
+
+export const Button = styled.button`
+  ${tw`
+  mt-5
+  px-5
+  py-2 
+  rounded-[12px] 
+  bg-orange-1 
+  text-brown-5 
+  font-semibold 
+  text-1.5lg 
+  md:text-2xl  
+  border-brown-5 
+  border-2 
+  transition 
+  duration-300 
+  ease-in-out 
+  active:transform 
+  active:translate-x-[3px] 
+  active:translate-y-[3px] 
+  active:shadow-none 
+  shadow-[3px 3px 0px #1d1d1d ]`}
+`
+export const MobileHeader = styled.div`
+  ${tw`flex flex-col justify-between items-center w-full px-7 my-10  md:hidden`}
+`
+export const MobileDate = styled.h1`
+  ${tw`text-brown-4 text-[23px] font-[600]`}
+`
