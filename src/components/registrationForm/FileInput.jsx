4855ec6bab167f228d6bbcaa7256c864bb2d7c1e
Ayush <135319056+ayussh-2@users.noreply.github.com>
2024-06-12 17:12:44 +0530

    feat : added registration page (#39)
    * feat:Added new site data and some minor changes
    
    * feat: added updated events section
    
    * chore: added text sizes from tailwind
    
    * feat: added Registartion page
    
    * chore:updated backgrounf image of Registerpage
    
    * chore: few fixes

---
+++ src/components/registrationForm/FileInput.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import {
+  Container,
+  HiddenInput,
+  FileName,
+  ImageButton,
+  Input,
+  InputContainer,
+  Label
+} from './FileInput.styles'
+import { imageBtn } from '../../config/content/registrationForm/Registration'
+import propTypes from 'prop-types'
+
+FileInput.propTypes = {
+  input: propTypes.shape({
+    id: propTypes.string.isRequired,
+    label: propTypes.string.isRequired,
+    type: propTypes.string.isRequired,
+    placeholder: propTypes.string.isRequired
+  }).isRequired
+}
+
+export default function FileInput({ input: { id, label, type, placeholder } }) {
+  const [fileName, setFileName] = useState(placeholder)
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0]
+    setFileName(file ? file.name : placeholder)
+  }
+
+  const handleClick = () => {
+    document.getElementById(id).click()
+  }
+
+  return (
+    <Container>
+      <HiddenInput type={type} id={id} onChange={handleFileChange} />
+      <Label htmlFor={id}>{label}</Label>
+      <InputContainer>
+        <Input onClick={handleClick}>
+          <label htmlFor={id}>
+            <ImageButton src={imageBtn} alt="upload" />
+          </label>
+          <FileName>{fileName}</FileName>
+        </Input>
+      </InputContainer>
+    </Container>
+  )
+}
---
+++ src/pages/Registration.jsx
@@ -0,0 +1,10 @@
+import RegistrationForm from '../components/registrationForm/RegistrationForm'
+function Registration() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  )
+}
+
+export default Registration
