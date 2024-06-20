442587955cc7a21eca122a8a14894db7150212ad
ayussh-2 <ayushatschool5@gmail.com>
2024-06-21 01:57:41 +0530

    fix:minor fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import About from '../components/about/About'
 import Events from '../components/events/Events'
 import NavHero from '../components/marginals/Nav_Hero/NavHero'
-// import { Sponsors } from '../components/sponsors/sponsors'
 import { TeamCarousel } from '../components/teamMembers/teamCarousel'
 import Footer from '../components/marginals/footer/Footer'
 import { Faq } from '../components/faq/faq'
-// import { RegistrationHelp } from '../components/registrationHelp/registrationhelp'
 import { Leads } from '../components/leads/leads'
 import { Schedule } from '../components/schedule/schedule'
 
@@ -15,11 +13,9 @@ const Home = () => {
       <NavHero />
       <About />
       <Events />
-      {/* <RegistrationHelp /> */}
       <Schedule />
       <Leads />
       <TeamCarousel />
-      {/* <Sponsors /> */}
       <Faq />
       <Footer />
     </div>
