ca0ecc1098ba3296b933e5b594e0d4dac39e1ff1
Harsh <140698710+Cybrite@users.noreply.github.com>
2024-06-20 20:11:36 +0530

    fix : updated content (#61)

--- src/config/content/registrationHelp/registrationHelp.js
+++ src/config/content/registrationHelp/registrationHelp.js
@@ -6,7 +6,7 @@ export const apostrophe = {
   quoteImage:
     'https://res.cloudinary.com/dhv234qct/image/upload/v1718100632/AICHE_SRC/team_members/quote-removebg-preview_ffkh8z.jpg',
   quote:
-    'Attention students! Registrations for Student Regional Conference 2023 are live! Click on the Register link given below. For the steps on how to register, click on the Help link. Join us for knowledge, in.',
+    'Attention students! Registrations for Student Regional Conference 2024 are live! Click on the Register link given below. For the steps on how to register, click on the Help link. Join us for knowledge, in.',
   quoteImageName: 'Prof. Sujit Sen',
   quoteImageDesignation: 'Principal Coordinator',
   organization: 'India Student Regional Conf. 2024'
