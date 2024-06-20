dc1b7f9fdc69936f6e5f01b56060c1b1e143eb47
ayussh-2 <ayushatschool5@gmail.com>
2024-06-21 01:49:14 +0530

    fix: footer fixes

--- src/components/marginals/Nav_Hero/Description.styles.jsx
+++ src/components/marginals/Nav_Hero/Description.styles.jsx
@@ -58,7 +58,8 @@ export const ButtonContainer = styled.div`
   md:max-w-[1000px] 
   h-full 
   flex 
-  md:flex-row 
+  flex-col
+  0.4xsm:flex-row 
   justify-center
   gap-8
   md:gap-[80px]
@@ -68,10 +69,11 @@ export const ButtonContainer = styled.div`
 
 export const HelpButton = styled.button`
   ${tw`
-  w-[140px]
-  h-[50px]
+  0.4xsm:w-[140px]
+  0.4xsm:h-[50px]
   px-2 
-  py-1 
+  py-2
+  0.4xsm:py-1 
   rounded-[11.53px] 
   bg-brown-2
   text-brown-5 
@@ -103,9 +105,8 @@ export const InnerSpan = styled.span`
 
 export const RegisterButton = styled.button`
   ${tw`
-  
   px-2 
-  py-1 
+  py-2
   rounded-[11.53px] 
   bg-orange-1 
   text-brown-5 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,19 +8,10 @@ import { Faq } from '../components/faq/faq'
 // import { RegistrationHelp } from '../components/registrationHelp/registrationhelp'
 import { Leads } from '../components/leads/leads'
 import { Schedule } from '../components/schedule/schedule'
-import { motion } from 'framer-motion'
+
 const Home = () => {
   return (
-    <motion.div
-      animate={{ opacity: 1, y: 0 }}
-      initial={{ opacity: 0, y: 500 }}
-      // exit={{ opacity: 0, y: 500 }}
-      transition={{
-        duration: 1,
-        ease: [0.2, 1, 0.2, 1],
-        delay: 0.5
-      }}
-    >
+    <div>
       <NavHero />
       <About />
       <Events />
@@ -31,7 +22,7 @@ const Home = () => {
       {/* <Sponsors /> */}
       <Faq />
       <Footer />
-    </motion.div>
+    </div>
   )
 }
 
