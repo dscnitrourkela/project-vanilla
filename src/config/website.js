7168699d49a739fd7d361ae06c661e70b3f22241
ayussh-2 <ayushatschool5@gmail.com>
2024-06-28 02:49:02 +0530

    feat: add SEO

---
+++ src/config/website.js
@@ -0,0 +1,38 @@
+/* eslint-disable*/
+
+const config = {
+  pathPrefix: '/',
+  siteTitle: 'SRC 2024',
+  siteTitleAlt: 'Official Website for SRC 2024',
+  siteTitleShort: 'SRC 2024',
+  siteUrl: 'https://sthitivartana.in',
+  siteLanguage: 'en',
+  siteLogo: '/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription:
+    "Held annually over three days, the Student Regional Conference (SRC) is organised by a student chapter of the American Institute of Chemical Engineers (AIChE) representing each of the 14 regions. This year's event will be organised by AIChE NITR, in continuation of the successful implementation put together by AIChE SVNIT the preceding year.",
+  author: 'SRC2024', // Author for schemaORGJSONLD
+  organization: 'SRC2024',
+
+  //   siteFBAppID: '100644471452393', // Facebook App ID
+  userTwitter: '@AicheNit', // Twitter Username
+  //   ogSiteName: 'HackNITR', // Facebook Site Name
+  ogLanguage: 'en_UK',
+  googleAnalyticsID: 'G-RDKTC8V88M',
+
+  // Manifest and Progress color
+  themeColor: '#ffffff',
+  backgroundColor: '#17171d',
+
+  // Social component
+  twitterHandle: '@AicheNit',
+  twitter: 'https://x.com/AicheNit',
+  x: 'https://x.com/AicheNit',
+  linkedin: 'https://www.linkedin.com/company/aiche-nitr',
+  youtube: 'https://www.youtube.com/@aichenitr4693',
+  facebook: 'https://www.facebook.com/AIChENITR',
+  instagram: 'https://www.instagram.com/aiche.nitrkl',
+  register: 'https://sthitivartana.in/register',
+  join: 'https://sthitivartana.in'
+}
+
+export default config
