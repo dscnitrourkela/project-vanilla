bb8a5f6774063a5545bbc85fba29f2b1ce58cee1
ayussh-2 <ayushatschool5@gmail.com>
2024-07-10 21:43:49 +0530

    fix: lint:fix

--- src/components/events/eventModal.styles.jsx
+++ src/components/events/eventModal.styles.jsx
@@ -13,6 +13,7 @@ const fadeInAnimation = keyframes`
 `
 export const Container = styled.main`
   ${tw`fixed inset-0 z-50 bg-brown-2 bg-opacity-75 backdrop-blur-md flex justify-center items-center w-screen px-[1rem] md:px-[10px]`}
+  animation: ${fadeInAnimation} 0.3s ease-out;
 `
 
 export const Section = styled.section`
