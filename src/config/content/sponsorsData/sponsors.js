d0f582b4b6d3fdc2c9ca3487ce27853aa899cf59
Dhrutinandan Swain <92504849+swaindhruti@users.noreply.github.com>
2024-08-04 13:05:22 +0530

    feat:Add sponsors section (#105)

--- src/config/content/sponsorsData/sponsors.js
+++ src/config/content/sponsorsData/sponsors.js
@@ -2,15 +2,15 @@ import tw from 'twin.macro'
 
 export default [
   {
-    styles: tw`max-md:m-[70px] md:w-[320px] md:h-[280px] md:m-[50px] `,
+    styles: tw`max-md:m-[30px] 0.5xxsm:h-[200px] 0.5xxsm:w-[240px] 0.5xxsm:mb-0.5 0.5xxsm:pr-2 md:w-[320px] md:h-[280px] md:m-[50px] `,
     url: 'https://res.cloudinary.com/dhv234qct/image/upload/v1722718317/AICHE_SRC/sponsors/sp2_foynsk.png'
   },
   {
-    styles: tw`max-md:m-[70px] md:w-[200px] md:h-[180px] md:m-[110px] `,
+    styles: tw`max-md:m-[70px] 0.5xxsm:h-[180px] 0.5xxsm:w-[200px] 0.5xxsm:mb-[40px] md:w-[200px] md:h-[180px] md:m-[110px] `,
     url: 'https://res.cloudinary.com/dhv234qct/image/upload/v1722718332/AICHE_SRC/sponsors/sp-2_uhuiyl.jpg'
   },
   {
-    styles: tw`max-md:m-[70px] md:w-[220px] md:h-[180px] md:m-[110px] `,
+    styles: tw`max-md:m-[70px] 0.5xxsm:h-[180px] 0.5xxsm:w-[200px] 0.5xxsm:mb-[70px] md:w-[220px] md:h-[180px] md:m-[110px] `,
     url: 'https://res.cloudinary.com/dhv234qct/image/upload/v1722718316/AICHE_SRC/sponsors/sp1_gwaalr.jpg'
   }
 ]
